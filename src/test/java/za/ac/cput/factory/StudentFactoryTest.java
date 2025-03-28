08fccf9d09e50916444ef357ee7ae57af7a1cfbb
bliksem <strathmoreyork@gmail.com>
2025-03-28 23:27:32 +0200

    Student factoryTest class

--- src/test/java/za/ac/cput/factory/StudentFactoryTest.java
+++ src/test/java/za/ac/cput/factory/StudentFactoryTest.java
@@ -1,15 +1,35 @@
 package za.ac.cput.factory;
 
-import za.ac.cput.domain.Student;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
+import static org.junit.Assert.*;
 public class StudentFactoryTest {
+    private String studentId;
+    private String studentName;
+    private String studentEmail;
+
+
+
+    @Before
+    public void setUp() throws Exception {
+        studentId = "12345";
+        studentName = "John Doe";
+        studentEmail = "john.doe@example.com";
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
     @Test
-    void testCreateStudent() {
-        Student student = StudentFactory.createStudent("S001", "John Doe", "john@example.com");
+    public void createStudent() {
+        Student student = StudentFactory.createStudent(studentId, studentName, studentEmail);
+
         assertNotNull(student);
-        assertEquals("S001", student.getId());
-        assertEquals("John Doe", student.getName());
+        assertEquals(studentId, student.getId());
+        assertEquals(studentName, student.getName());
+        assertEquals(studentEmail, student.getEmail());
     }
 }
